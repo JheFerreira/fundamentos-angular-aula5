626b285b1a109947138dcf85027be544a52542e5
Jessica Ferreira <jheferreiramath@gmail.com>
2023-03-01 07:10:25 -0300

    Aula5D concluida

--- roteamento/src/app/app.module.ts
+++ roteamento/src/app/app.module.ts
@@ -1,22 +1,32 @@
 import { NgModule } from '@angular/core';
+
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
 import { AppRoutingModule } from './app-routing.module';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.component';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
+import { PaginaComParametrosComponent } from './pagina-com-parametros/pagina-com-parametros.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent, 
-    PrimeiraPaginaComponent
-  ],
-  imports: [
-    RouterModule
-    
-  
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+declarations: [
+AppComponent,
+PrimeiraPaginaComponent,
+SegundaPaginaComponent,
+PaginaNaoEncontradaComponent,
+PaginaComParametrosComponent
+],
+imports: [
+
+AppRoutingModule,
+RouterModule,
+BrowserModule
+
+
+],
+providers: [],
+bootstrap: [AppComponent]
 })
 export class AppModule { }
